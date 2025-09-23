86c72a2aeb2e93d7886b0cd645f616152ac13752
pacopul <profesor.pacopul@iesazarquiel.es>
2025-09-23 11:22:09 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PokemonReact/',
+
   build: {
     outDir: 'docs', // Carpeta de salida configurada como 'docs'
   },
